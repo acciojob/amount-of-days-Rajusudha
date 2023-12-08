9c3f2a3757c4933615ace576e5a48a7fc4af67ba
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-12-08 09:31:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+  if (isLeapYear(year)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
+
+// Helper function to check if a year is a leap year
+function isLeapYear(year) {
+  // Leap years are divisible by 4, but not divisible by 100 unless they are also divisible by 400
+  return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+}
+
+// Examples
+console.log(daysOfAYear(2022)); // 365
+console.log(daysOfAYear(2024)); // 366
+console.log(daysOfAYear(1900)); // 365
+console.log(daysOfAYear(2000)); // 366
